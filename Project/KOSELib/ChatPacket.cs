489a24f04b5db8f86bc7e36bf8fe93930fb82b0b
fkose3 <fkose3@gmail.com>
2013-09-20 17:48:21 +0300

    Chat yaperken kullanılacak kütüp hanenin olusturulmasına baslandı

---
+++ Project/KOSELib/ChatPacket.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KOSELib
+{
+    public static class ChatPacket
+    {
+        // Text i şifrele 
+        private static void IsCrypto(ref string text)
+        {
+            string OldText = text;
+            text = "";
+
+            int upper = 3;
+            foreach (char isn in OldText.ToCharArray())
+            {
+                string chr = "";
+                for (int i = 0; i < 3; i++)
+                {
+                    chr = Convert.ToString(isn + upper);
+                }
+                text += chr;
+                upper++;
+            }
+
+        }
+        // Şifreyi çöz
+        private static void DeCrypto(ref string text)
+        {
+            string OldText = text;
+            text = "";
+
+            int upper = 3;
+            foreach (char isn in OldText.ToCharArray())
+            {
+                string chr = "";
+                for (int i = 0; i < 3; i++)
+                {
+                    chr = Convert.ToString(isn - upper);
+                }
+                text += chr;
+                upper++;
+            }
+        }
+
+        // Texti byte yap
+        private static byte[] TextChBytes(string text)
+        {
+            return Encoding.UTF8.GetBytes(text);
+        }
+        // Byte Text yap
+        private static string ByteChText(byte[] bytes)
+        {
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        // Yazıyı pakete ekle
+        public static void PacketAddText(ref byte[] pBuf, string text, ref int index)
+        {
+            IsCrypto(ref text);
+
+            Global.SetShort(ref pBuf, (short)text.Length,ref index);
+
+            byte[] chat = TextChBytes(text);
+
+            Global.SetString(ref pBuf, chat, text.Length, ref index);
+        }
+
+        // Yazıyı paketten kaldır
+        public static string PacketReadText(byte[] pBuf, ref int index)
+        {
+            byte[] chat = null;
+
+            Global.GetString(pBuf, ref chat, Global.GetShort(pBuf, ref index), ref index);
+
+            string text = ByteChText(chat);
+
+            DeCrypto(ref text);
+            
+            return text;
+        }
+
+    }
+}
---
+++ Project/KOSELib/Global.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KOSELib
+{
+    public static class Global
+    {
+        public const bool isClient = true;
+        public const bool isServer = false;
+
+        public static void SetShort(ref byte[] send_buff, Int16 tem, ref int send_index) 
+        {
+            byte[] NewByte = { (byte)(tem & 0x00FF), (byte)((tem & 0xFF00) >> 8) };
+
+            for (int i = 0; i < 2; i++)
+                send_buff[++send_index] = NewByte[i];
+
+        }
+
+        public static short GetShort(byte[] send_buff, ref int send_index) 
+        {
+            short tmp = Convert.ToInt16(send_buff[++send_index]
+                              | ((send_buff[++send_index]) << 8));
+
+            return tmp;
+        }
+
+        public static void SetString(ref byte[] send_buff, byte[] text, int text_len, ref int send_index) 
+        {
+            for (int i = 0; i < text_len; i++)
+                send_buff[++send_index] = text[i];
+        }
+        
+        public static void GetString(byte[] get_buff, ref byte[] text_buff, int text_len, ref int get_index) 
+        {
+            int text_index = -1;
+            for (int i = 0; i < text_len; i++)
+                text_buff[++text_index] = get_buff[++get_index];
+        }
+    }
+}
